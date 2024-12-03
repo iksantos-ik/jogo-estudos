e370946fad28c6ef4a2f9c07a5d7fc135b8ea942
Ilana <ilanakssantos@gmail.com>
2024-12-03 18:28:59 -0300

    joguinho para estudar

---
+++ app.js
@@ -0,0 +1,14 @@
+alert('Olá, tudo bem?')
+
+
+let nome = prompt('Digite seu nome');
+let numero = prompt('Digite um numero');
+let contador = 0;
+
+while (contador <= numero){
+    alert(`${contador}`)
+    contador++
+}
+
+
+
